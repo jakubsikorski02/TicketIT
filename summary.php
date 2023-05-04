dd474b39d3ef97254a0ca3d9d81466d4b40c992f
Jakub <130572162+jakubsikorski02@users.noreply.github.com>
2023-05-04 22:35:12 +0200

    Update summary.php

--- summary.php
+++ summary.php
@@ -16,6 +16,10 @@
     <div class="main-container">
         <h1>Order Summary</h1>
         <?php
+            if(!isset($_SESSION['userId'])){
+              header("Location: login.php");
+          }
+          else{
         $summaryId = $_SESSION["insertedId"];
         $sql = "SELECT movies.title, seats.row_number, seats.seat_number, schedule.date, schedule.hour, cinema_hall.hall_name 
             FROM movies, seats, schedule, booked, cinema_hall 
@@ -36,6 +40,7 @@
         } else {
             echo '<a href="movie-list.php">Book now!</a>';
         }
+       }
         ?>
     </div>
     <div class="footer">
